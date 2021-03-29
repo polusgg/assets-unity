4c2fdf33c2e1e878751fb67c4d54aee056eb983e
cybershard <ashwatdd01@gmail.com>
2021-03-29 15:01:39 -0400

    [Editor] Asset Bundle Resource generation

---
+++ Assets/Editor/AssetBundleResource.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Assets.Editor {
+    [CreateAssetMenu(fileName = "AssetBundleResource", menuName = "Create Resource - Asset Bundle", order = 0)]
+    public class AssetBundleResource : ScriptableObject {
+        public uint BaseId;
+        public Object[] Assets;
+    }
+}
---
+++ Assets/Editor/AssetBundleResourceEditor.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor {
+    [CustomEditor(typeof(AssetBundleResource))]
+    public class AssetBundleResourceEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            base.OnInspectorGUI();
+
+            if (GUILayout.Button("Build")) {
+                if (!Directory.Exists("Assets/AssetBundles"))
+                	Directory.CreateDirectory("Assets/AssetBundles");
+
+                AssetBundleResource resource = (AssetBundleResource) serializedObject.targetObject;
+
+                if (resource.Assets.Length == 0)
+                    return;
+                
+                var jsonPath = $"Assets/AssetBundles/{resource.name}-Manifest.json";
+                File.WriteAllText(jsonPath, JsonUtility.ToJson(
+                    new SerializableAssetBundleResource(resource)
+                ));
+
+                BuildPipeline.BuildAssetBundles("Assets/AssetBundles",
+                    new[] {
+                        new AssetBundleBuild {
+                            assetNames = resource.Assets.Select(AssetDatabase.GetAssetPath).Append(jsonPath).ToArray(),
+                            assetBundleName = resource.name
+                        }
+                    },
+                    BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows
+                );
+            }
+        }
+
+        private struct SerializableAssetBundleResource {
+            private uint BaseId;
+            private string[] Assets;
+
+            public SerializableAssetBundleResource(AssetBundleResource assetBundleResource) {
+                BaseId = assetBundleResource.BaseId;
+                Assets = assetBundleResource.Assets.Select(AssetDatabase.GetAssetPath).ToArray();
+            }
+        }
+    }
+}
